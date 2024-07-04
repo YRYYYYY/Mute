51b688371d8b2a6281481937d9c28d216c3fd0d2
YRYYYYY <92637997+YRYYYYY@users.noreply.github.com>
2024-07-04 07:40:04 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import os
+from telethon import TelegramClient, events
+
+api_id = os.environ['API_ID']
+api_hash = os.environ['API_HASH']
+session_string = os.environ['TELETHON_SESSION']
+admin_ids = [int(i) for i in os.environ['admins'].split(",")]
+client = TelegramClient(StringSession(session_string), api_id, api_hash)
+
+@client.on(events.NewMessage(pattern='/mute'))
+async def mute_user(event):
+    
+    if event.sender_id in admin_ids:
+        if event.is_group and event.reply_to_msg_id:
+            replied_message = await event.get_reply_message()
+            user_id_to_mute = replied_message.sender_id
+            
+            await event.reply(f"User {user_id_to_mute} has been muted in this chat.")
+
+@client.on(events.NewMessage(pattern='/unmute'))
+async def unmute_user(event):
+    
+    if event.sender_id in admin_ids:
+        if event.is_group and event.reply_to_msg_id:
+            replied_message = await event.get_reply_message()
+            user_id_to_unmute = replied_message.sender_id
+           
+            await event.reply(f"User {user_id_to_mute} has been unmuted in this chat.")
+
+
+client.start()
+client.run_until_disconnected()
