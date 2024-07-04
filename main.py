aea4120c1529f643105f4e8cfdd0955df76ff056
YRYYYYY <92637997+YRYYYYY@users.noreply.github.com>
2024-07-04 09:48:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,25 +9,35 @@ session_string = os.environ['TELETHON_SESSION']
 admin_ids = [int(i.strip()) for i in os.environ['admins'].split(",")]
 client = TelegramClient(StringSession(session_string), api_id, api_hash)
 
-@client.on(events.NewMessage(pattern='/mute'))
-async def mute_user(event):
-    
-    if event.sender_id in admin_ids:
-        if event.is_group and event.reply_to_msg_id:
-            replied_message = await event.get_reply_message()
-            user_id_to_mute = replied_message.sender_id
-            
-            await event.reply(f"User {user_id_to_mute} has been muted in this chat.")
+users_to_delete_messages = {}
 
-@client.on(events.NewMessage(pattern='/unmute'))
-async def unmute_user(event):
-    
+@client.on(events.NewMessage(chats=events.ChatType.GROUP))
+async def delete_user_message(event):
+    if event.sender_id in users_to_delete_messages:
+        user_id = event.sender_id
+        await event.delete()
+        
+
+@client.on(events.NewMessage(chats=events.ChatType.GROUP, pattern='/mute'))
+async def add_user_to_delete_list(event):
     if event.sender_id in admin_ids:
-        if event.is_group and event.reply_to_msg_id:
+        if event.is_private and event.is_reply:
             replied_message = await event.get_reply_message()
-            user_id_to_unmute = replied_message.sender_id
-           
-            await event.reply(f"User {user_id_to_unmute} has been unmuted in this chat.")
+            user_id = replied_message.sender_id
+            users_to_delete_messages[user_id] = True
+            await event.reply(f"User {user_id} has been muted in this chat.")
+
+@client.on(events.NewMessage(chats=events.ChatType.GROUP, pattern='/unmute'))
+async def remove_user_from_delete_list(event):
+    if event.sender_id in admin_ids:
+        if event.is_private and event.is_reply:
+            replied_message = await event.get_reply_message()
+            user_id = replied_message.sender_id
+            if user_id in users_to_delete_messages:
+                del users_to_delete_messages[user_id]
+                await event.reply(f"User {user_id} has been unmuted in this chat.")
+            else:
+                await event.reply("User is not in delete list.")
 
 client.start()
 client.run_until_disconnected()
